b05dfd3b923bbce19327a94079caf6803a3071cd
guoguokkk <1501056593@qq.com>
2019-03-10 21:36:02 +0800

    Update BucketSort.cpp

--- Sort/BucketSort.cpp
+++ Sort/BucketSort.cpp
@@ -4,23 +4,81 @@ struct ListNode
 {
 	int val;
 	ListNode* next;
-	ListNode(int val) :val(val), next(NULL) {}
+	ListNode(int val = 0) :val(val), next(NULL) {}
 };
+
+ListNode* Merge(ListNode* head, ListNode* curHead)
+{
+	ListNode* tempHead = new ListNode(0);
+	ListNode* temp = tempHead;
+	while (head != NULL && curHead != NULL)
+	{
+		if (head->val < curHead->val)
+		{
+			temp->next = head;
+			head = head->next;
+		}
+		else
+		{
+			temp->next = curHead;
+			curHead = curHead->next;
+		}
+		temp = temp->next;
+	}
+
+	if (head != NULL) { temp->next = head; }
+	if (curHead != NULL) { temp->next = curHead; }
+	return tempHead->next;
+}
+
+void Insert(ListNode*& head, int data)//æŒ‡é’ˆçš„å¼•ç”¨
+{
+	ListNode* newHead = new ListNode(data);
+	newHead->next = head;
+	if (head == NULL) { head=newHead; }
+
+	ListNode* pre = newHead;
+	ListNode* cur = pre->next;
+	while (cur != NULL)
+	{
+		if (cur->val > data) { break; }
+		pre = cur;
+		cur = cur->next;
+	}
+
+	if (pre == newHead) { head=newHead; }
+	pre->next = newHead;
+	newHead->next = cur;
+}
 
 void BucketSort(int * nums, int n)
 {
 	if (n <= 1) { return; }
 
-	//ÏÈ°ÑËùÓÐµÄÊý¾Ý¶¼·Öµ½¸÷¸öÍ°ÖÐ
+	//å…ˆæŠŠæ‰€æœ‰çš„æ•°æ®éƒ½åˆ†åˆ°å„ä¸ªæ¡¶ä¸­
 	int bucket_num = 10;
 	ListNode** bucket = new ListNode*[bucket_num] {};
 	for (int i = 0; i < n; ++i)
 	{
-		int index = nums[i] % bucket_num;
-		ListNode* index_bucket_head = bucket[index];
-
+		int index = nums[i] / bucket_num;
+		Insert(bucket[index], nums[i]);
 	}
 
+	//åˆå¹¶
+	ListNode* head = bucket[0];
+	for (int i = 1; i < bucket_num; ++i)
+	{
+		Merge(head, bucket[i]);
+	}
+
+	//æŠŠæ•°å­—å­˜åˆ°æ•°ç»„
+	int i = 0;
+	ListNode* temp = head;
+	while (temp != NULL)
+	{
+		nums[i++] = temp->val;
+		temp = temp->next;
+	}
 	delete[] bucket;
 }
 
