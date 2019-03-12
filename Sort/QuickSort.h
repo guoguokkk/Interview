87dbee2a4ab9597625dfd62c01dbe1483b2a634d
guoguokkk <1501056593@qq.com>
2019-03-12 20:41:14 +0800

    Update QuickSort.h

--- Sort/QuickSort.h
+++ Sort/QuickSort.h
@@ -1,5 +1,6 @@
 #ifndef QUICKSORT_H
-void QuickSort(int* nums, int low, int high);//�ݹ�
+#define QUICKSORT_H
+void QuickSort(int* nums, int low, int high);//µÝ¹é
 template<typename T> void QuickSortTemplate(T* nums, int low, int high);
 template<typename T> void QuickSortTemplate1(T* nums, int n);
 void Test_QuickSort();
